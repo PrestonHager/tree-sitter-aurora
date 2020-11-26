17e3417e62cac416eb564e4ec1ebcf100934278d
Preston Hager <preston@hagerfamily.com>
2020-11-25 18:19:19 -0800

    Something.

--- build/Release/.deps/Release/obj.target/tree_sitter_Aurora_binding/src/binding.o.d
+++ build/Release/.deps/Release/obj.target/tree_sitter_Aurora_binding/src/binding.o.d
@@ -1,22 +1,22 @@
-cmd_Release/obj.target/tree_sitter_Aurora_binding/src/binding.o := c++ -o Release/obj.target/tree_sitter_Aurora_binding/src/binding.o ../src/binding.cc '-DNODE_GYP_MODULE_NAME=tree_sitter_Aurora_binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node -I/Users/prestonhager/Library/Caches/node-gyp/14.5.0/src -I/Users/prestonhager/Library/Caches/node-gyp/14.5.0/deps/openssl/config -I/Users/prestonhager/Library/Caches/node-gyp/14.5.0/deps/openssl/openssl/include -I/Users/prestonhager/Library/Caches/node-gyp/14.5.0/deps/uv/include -I/Users/prestonhager/Library/Caches/node-gyp/14.5.0/deps/zlib -I/Users/prestonhager/Library/Caches/node-gyp/14.5.0/deps/v8/include -I../node_modules/nan -I../src  -O3 -gdwarf-2 -mmacosx-version-min=10.13 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/tree_sitter_Aurora_binding/src/binding.o.d.raw   -c
+cmd_Release/obj.target/tree_sitter_Aurora_binding/src/binding.o := c++ '-DNODE_GYP_MODULE_NAME=tree_sitter_Aurora_binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node -I/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/src -I/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/deps/openssl/config -I/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/deps/openssl/openssl/include -I/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/deps/uv/include -I/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/deps/zlib -I/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/deps/v8/include -I../node_modules/nan -I../src  -Os -gdwarf-2 -mmacosx-version-min=10.10 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/tree_sitter_Aurora_binding/src/binding.o.d.raw   -c -o Release/obj.target/tree_sitter_Aurora_binding/src/binding.o ../src/binding.cc
 Release/obj.target/tree_sitter_Aurora_binding/src/binding.o: \
   ../src/binding.cc ../src/tree_sitter/parser.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/node.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/v8.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/v8-internal.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/v8-version.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/v8config.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/v8-platform.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/node_version.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/node.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/v8.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/v8-internal.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/v8-version.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/v8config.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/v8-platform.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/node_version.h \
   ../node_modules/nan/nan.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv/errno.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv/version.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv/unix.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv/threadpool.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv/darwin.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/node_buffer.h \
-  /Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/node_object_wrap.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv/errno.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv/version.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv/unix.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv/threadpool.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv/darwin.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/node_buffer.h \
+  /Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/node_object_wrap.h \
   ../node_modules/nan/nan_callbacks.h \
   ../node_modules/nan/nan_callbacks_12_inl.h \
   ../node_modules/nan/nan_maybe_43_inl.h \
@@ -31,22 +31,22 @@ Release/obj.target/tree_sitter_Aurora_binding/src/binding.o: \
   ../node_modules/nan/nan_json.h
 ../src/binding.cc:
 ../src/tree_sitter/parser.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/node.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/v8.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/v8-internal.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/v8-version.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/v8config.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/v8-platform.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/node_version.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/node.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/v8.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/v8-internal.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/v8-version.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/v8config.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/v8-platform.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/node_version.h:
 ../node_modules/nan/nan.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv/errno.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv/version.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv/unix.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv/threadpool.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/uv/darwin.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/node_buffer.h:
-/Users/prestonhager/Library/Caches/node-gyp/14.5.0/include/node/node_object_wrap.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv/errno.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv/version.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv/unix.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv/threadpool.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/uv/darwin.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/node_buffer.h:
+/Users/prestonhager/.atom/.node-gyp/Library/Caches/node-gyp/6.1.12/include/node/node_object_wrap.h:
 ../node_modules/nan/nan_callbacks.h:
 ../node_modules/nan/nan_callbacks_12_inl.h:
 ../node_modules/nan/nan_maybe_43_inl.h:
